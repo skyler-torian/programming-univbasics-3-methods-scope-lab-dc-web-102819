dc2d179a0b07eb4c3ab3cc8f1b05e52b4b83368a
Skyler Torian <skymanat7@yahoo.com>
2019-10-10 16:48:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,8 +4,8 @@ def mario
 end
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
-  puts status
+  phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts phrase
 end
 
 def link 
@@ -13,7 +13,7 @@ def link
   puts phrase
 end
 
-def any_phrase
+def star_fox
   phrase = "Do A Barrel Roll!"
   puts phrase 
 end
